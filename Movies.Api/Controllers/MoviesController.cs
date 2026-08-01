56b6baa13989bb5d953e1c223d816aea2bb4ee3d
Tushar <tusharforcourses@gmail.com>
2026-08-02 00:05:38 +0530

    implement slug retrieval

--- Movies.Api/Controllers/MoviesController.cs
+++ Movies.Api/Controllers/MoviesController.cs
@@ -21,13 +21,13 @@ public class MoviesController : ControllerBase
         var movie = request.MapToMovie();
         await _movieRepository.CreateAsync(movie);
 		var movieResponse = movie.MapToResponse();
-        return CreatedAtAction(nameof(Get), new { id = movie.Id }, movieResponse);
+        return CreatedAtAction(nameof(Get), new { idOrSlug = movie.Id }, movieResponse);
     }
 
     [HttpGet(ApiEndpoints.Movies.Get)]
-    public async Task<IActionResult> Get([FromRoute] Guid id)
+    public async Task<IActionResult> Get([FromRoute] string idOrSlug)
     {
-        var movie = await _movieRepository.GetByIdAsync(id);
+        var movie = Guid.TryParse(idOrSlug, out var id) ?  await _movieRepository.GetByIdAsync(id) : await _movieRepository.GetBySlugAsync(idOrSlug);
         if (movie is null)
         {
             return NotFound();
